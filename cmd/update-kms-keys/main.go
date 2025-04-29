34c024d0342d744662e5a3555fb2d44cf468f92d
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2025-04-28 21:37:56 -0400

    Cleanup script

--- cmd/update-kms-keys/main.go
+++ cmd/update-kms-keys/main.go
@@ -1,64 +1,38 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
 	j "encoding/json"
 	"fmt"
 	h "net/http"
+	"os"
 
 	"github.com/google/uuid"
-	"github.com/onflow/flow-go-sdk"
+	// "github.com/onflow/flow-go-sdk"
 )
 
+// 0xfaaecfd784e1508a
 func main() {
-	addNewKey()
+	args := os.Args
+	fmt.Println("args: ", args[1])
+	if args == nil || len(args) < 2 {
+		fmt.Println("No account address provided provided")
+		return
+	}
+	address := os.Args[1]
+	addNewKey(address)
 }
 
-type ReqBody struct {
-	Address string `json:"address"`
-}
-
-type ResponseBody struct {
-	Address   string `json:"address"`
-	Keys      []Key  `json:"keys"`
-	Type      string `json:"type"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
-}
-
-type Key struct {
-	Index     int    `json:"index"`
-	Type      string `json:"type"`
-	PublicKey string `json:"publicKey"`
-	SignAlgo  string `json:"signAlgo"`
-	HashAlgo  string `json:"hashAlgo"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
-}
-
-func addNewKey() {
+func addNewKey(accountAddress string) {
 	fmt.Println("Running Sync Keys to add new keys to the account")
 
-	accountAddress := "0xfaaecfd784e1508a"
-	acctAddr := flow.HexToAddress(accountAddress) // this is a flow.Address
-
-	b := ReqBody{Address: acctAddr.Hex()}
-
-	idempotencyKey := uuid.New().String()
-
-	bodyAsJson, jsonErr := j.Marshal(b)
-	if jsonErr != nil {
-		fmt.Println("Error marshaling JSON:", jsonErr)
-	}
-
-	bodyReader := bytes.NewReader(bodyAsJson)
-
-	req, reqErr := h.NewRequest("POST", "http://localhost:3005/v1/accounts/"+accountAddress+"/add-new-key", bodyReader)
+	req, reqErr := h.NewRequest("POST", "http://localhost:3005/v1/accounts/"+accountAddress+"/add-new-key", nil)
 	if reqErr != nil {
 		fmt.Println("Error:", reqErr)
 		return
 	}
 
+	idempotencyKey := uuid.New().String()
 	req.Header.Add("Idempotency-Key", idempotencyKey)
 	req.Header.Add("Content-Type", "application/json")
 
@@ -83,5 +57,30 @@ func addNewKey() {
 		return
 	}
 
-	fmt.Println("Response Body: ", body)
+	fmt.Printf("A new key was added to account %s\n", body.Address)
+	newKey := body.Keys[len(body.Keys)-1]
+	fmt.Printf("New Public Key: %s\n", newKey.PublicKey)
+	fmt.Printf("Key Index: %d\n", newKey.Index)
+}
+
+type ReqBody struct {
+	Address string `json:"address"`
+}
+
+type ResponseBody struct {
+	Address   string `json:"address"`
+	Keys      []Key  `json:"keys"`
+	Type      string `json:"type"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
+}
+
+type Key struct {
+	Index     int    `json:"index"`
+	Type      string `json:"type"`
+	PublicKey string `json:"publicKey"`
+	SignAlgo  string `json:"signAlgo"`
+	HashAlgo  string `json:"hashAlgo"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
 }
